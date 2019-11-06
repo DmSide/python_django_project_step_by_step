9bf26289933622205337bfb40f6aee6292e82419
Dmitriy Sidenko <dsidenko@zuzex.com>
2019-11-06 17:09:39 +0300

    Change config to nlp_tasks and run_tasks

--- test_django/test_django/settings.py
+++ test_django/test_django/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 'run_tasks',
+    # 'nlp_tasks'
+    'polls',
     'background_task'
 ]
 
