93dd50c66e88f4444f44f57aabb1a840d2c6fd55
Dmitriy Sidenko <dsidenko@zuzex.com>
2019-11-06 12:08:54 +0300

    Add BT to django project

--- test_django/polls/views.py
+++ test_django/polls/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from background_task import background
 # Create your views here.
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+@background(schedule=5)
+def hello():
+    print("Hello World frm BT!")
+
+
+def background_view(request):
+    hello()
+    return HttpResponse("Hello world! Our BT scripts works in async")
